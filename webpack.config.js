a843005c7ac478a7c8720f1f2cf86227f3f8d8e8
Ivan Storck <ivanoats@gmail.com>
2018-12-07 23:15:54 +0000

    boilerplate for basic react

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist',
+    host: '0.0.0.0',
+    allowedHosts: [
+      '.ivanstorck.com'
+    ],
+    disableHostCheck: true
+  }
+}
